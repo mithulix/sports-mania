cb9b9e2568e755e9f5bdf8f08ae867c617b19b76
mithulix <mohaiminul.devs@gmail.com>
2022-10-12 00:00:00 +0600

    2022-10-12T00:00:00+06:00

---
+++ graph.js
@@ -0,0 +1,31 @@
+const jsonfile = require("jsonfile");
+const moment = require("moment");
+const simpleGit = require("simple-git");
+const random = require("random");
+
+const FILE_PATH = "./data.json";
+
+
+const makeCommit = n => {
+  if (n === 0) return simpleGit().push();
+  const x = random.int(0, 1);
+  const y = random.int(0, 1);
+  const DATE = moment("2022-10-10")
+    .add(x, "days")
+    .add(y, "hours")
+    .subtract(0, "days")
+    .add(1, "days")
+    .format();
+  const data = {
+    date: DATE,
+  };
+  console.log(DATE );
+  jsonfile.writeFile(FILE_PATH, data, () => {
+    simpleGit()
+      .add([FILE_PATH])
+      .commit(DATE, { "--date": DATE }, makeCommit.bind(this, --n))
+      .push();
+  });
+};
+
+makeCommit(10);
